a58dbe5acc3bbad19bc595354f703a18e541765d
gray5246 <gchatham.f21@warren-wilson.edu>
2021-11-29 18:48:07 -0500

    jk

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 if __name__ == "__main__":
     from pyo import *
+    from random import random
+    from mido import MidiFile
+    class Synth:
+        def __init__(self, transpo=1, mul=1):
+            self.transpo = Sig(transpo)
+            self.note = Notein(poly=10, scale=1, first=0, last=127)
+
+            self.pit = self.note["pitch"] * self.transpo
+            self.amp = MidiAdsr(self.note["velocity"], attack=0.001, decay=0.05, sustain=0.7, release=0.5, mul=0.1, )
+
+            self.osc1 = LFO(self.pit, sharp=0.5, type=2, mul=self.amp).mix(1)
+            self.osc2 = LFO(self.pit * 0.997, sharp=0.5, type=2, mul=self.amp).mix(1)
+
+            self.mix = Mix([self.osc1, self.osc2], voices=2)
+
+            self.damp = ButLP(self.mix, freq=5000)
+
+            self.lfo = Sine(0.2, phase=[random(), random()]).range(250, 4000)
+            self.notch = ButBR(self.damp, self.lfo, mul=mul)
+
+        def out(self):
+            self.notch.out()
+            return self
+
+        def sig(self):
+            return self.notch
     s = Server()
-    s.setMidiInputDevice(0)
+    s.setMidiInputDevice(99)
     s.boot()
     s.start()
-    s.amp = 0.1
-    midi = Notein()
-    pitch = MToF(midi['pitch'])
-    amp = MidiAdsr(midi['velocity'])
-    wave = SquareTable()
-    oscs = Osc(wave, mul=amp)
-    d = Delay(oscs, delay=0.25, feedback=0, maxdelay=1, mul=1, add=0)
-    d.ctrl(title='Delay')
-    r = Freeverb(oscs, size=[.79, .8], damp=.9, bal=.3)
-    r.ctrl(title='Reverb')
+    synt = Synth().out()
+    synt.note = (MidiFile("Data/sun.mid"))
+    for message in synt.note.play():
+        s.addMidiEvent(*message.bytes())
     s.gui(locals())
 
 
